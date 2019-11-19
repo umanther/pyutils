baf0eda8ced25d6f4d6761879ba844262f473f57
Geoff Hellstrand <ubil@hotmail.com>
2019-11-19 14:23:03 -0800

    Updated module imports Renamed SEVERITIES constant to SC_SEVERITIES for clarity
    Signed-off-by: Geoffrey Hellstrand <ubil@hotmail.com>

--- ubiltools/__init__.py
+++ ubiltools/__init__.py
@@ -2,6 +2,7 @@ from ubiltools.decorators import *
 from ubiltools.getkey import *
 from ubiltools.term_colors import *
 from ubiltools.structures import *
+from ubiltools.sc_support import *
 
 __version__ = '1.0.3'
 __author__ = 'Geoff Hellstrand <ubil@hotmail.com>'
--- ubiltools/sc_support.py
+++ ubiltools/sc_support.py
@@ -1,8 +1,8 @@
 from ubiltools.structures import RevDict as _RevDict
 
-SEVERITIES: _RevDict = _RevDict()
+SC_SEVERITIES: _RevDict = _RevDict()
 
-SEVERITIES.update({0: 'Info',
+SC_SEVERITIES.update({0: 'Info',
                    1: 'Low',
                    2: 'Medium',
                    3: 'High',
